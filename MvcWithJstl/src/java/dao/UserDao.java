b190d15689eb8b108fa372fe41c2e372ba990b90
khalid7487 <khalid15-7487@diu.edu.bd>
2019-10-16 20:56:42 +0600

    user dao

--- MvcWithJstl/src/java/dao/UserDao.java
+++ MvcWithJstl/src/java/dao/UserDao.java
@@ -14,5 +14,94 @@ import java.util.*;
 import model.User;
 import util.Database;
 public class UserDao {
+    private Connection connection;
+    public UserDao(){
+        connection=Database.getConnection();
+    }
+    public void checkUser(User user){
+        try {
+            PreparedStatement ps=connection.prepareStatement("select uname from users where uname = ?");
+            ps.setString(1, user.getUname());
+            ResultSet rs=ps.executeQuery();
+            if(rs.next()){
+                updateUser(user);
+            }else{
+                addUser(user);
+            }
+        } catch (Exception e) {
+            System.out.println("Error in check() -->" + e.getMessage());
+
+        }
+    }
+    public void addUser(User user){
+        try {
+            PreparedStatement ps=connection.prepareStatement("insert into users(uname, password, email, registeredon) values (?, ?, ?, ? )");
+            ps.setString(1, user.getUname());
+            ps.setString(2, user.getPassword());
+            ps.setString(3, user.getEmail());
+            ps.setDate(4, new java.sql.Date(user.getRegisteredon().getTime()));
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void deleteUser(String userId){
+        try {
+            PreparedStatement ps=connection.prepareStatement("delete from users where uname=?");
+            ps.setString(1, userId);
+            ps.executeUpdate();
+            
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public void updateUser(User user){
+        try {
+            PreparedStatement ps=connection.prepareStatement("update users set password=?, email=?, registeredon=?"
+                    + "where uname=?");
+            ps.setString(1, user.getPassword());
+            ps.setString(2, user.getEmail());
+            ps.setDate(3, new java.sql.Date(user.getRegisteredon().getTime()));
+            ps.setString(4, user.getUname());
+            ps.executeUpdate();
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+    public List<User> getAllUsers(){
+        List<User> users=new ArrayList<User>();
+        try {
+            Statement statement=connection.createStatement();
+            ResultSet rs=statement.executeQuery("select * from users");
+            while(rs.next()){
+                User user=new User();
+                user.setUname(rs.getString("uname"));
+                user.setPassword(rs.getString("password"));
+                user.setEmail(rs.getString("email"));
+                user.setRegisteredon(rs.getDate("registeredon"));
+                users.add(user);
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return users;
+    }
     
+    public User getUserById(String userId){
+        User user=new User();
+        try {
+            PreparedStatement ps=connection.prepareStatement("select * from users where uname=?");
+            ps.setString(1, userId);
+            ResultSet rs=ps.executeQuery();
+            if(rs.next()){
+                user.setUname(rs.getString("uname"));
+                user.setPassword(rs.getString("password"));
+                user.setEmail(rs.getString("email"));
+                user.setRegisteredon(rs.getDate("registeredon"));
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return user;
+    }
 }
