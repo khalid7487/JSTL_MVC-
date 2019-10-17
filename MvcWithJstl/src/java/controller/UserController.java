81b3f8911947fb6b747e83504515e3862e715e5a
khalid7487 <khalid15-7487@diu.edu.bd>
2019-10-17 11:42:00 +0600

    controller

---
+++ MvcWithJstl/src/java/controller/UserController.java
@@ -0,0 +1,83 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package controller;
+
+import dao.UserDao;
+import java.io.IOException;
+import java.io.PrintWriter;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+import javax.servlet.RequestDispatcher;
+import javax.servlet.ServletException;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+import model.User;
+
+/**
+ *
+ * @author KHALID
+ */
+public class UserController extends HttpServlet {
+
+    private static final long serialVersionUID=1L;
+    private static String INSERT_OR_EDIT="";
+    private static String LIST_USER="";
+    private UserDao dao;
+    
+    public UserController(){
+        super();
+        dao=new UserDao();
+    }
+  
+    @Override
+    protected void doGet(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+        String forward="";
+        String action=request.getParameter("action");
+        if(action.equalsIgnoreCase("delete")){
+            String userId=request.getParameter("userId");
+            dao.deleteUser(userId);
+            forward=LIST_USER;
+            request.setAttribute("users", dao.getAllUsers());
+        }else if(action.equalsIgnoreCase("edit")){
+            forward=INSERT_OR_EDIT;
+            String userId=request.getParameter("userId");
+            User user=dao.getUserById(userId);
+            request.setAttribute("user", user);
+        }else if(action.equalsIgnoreCase("listUser")){
+            forward=LIST_USER;
+            request.setAttribute("users", dao.getAllUsers());
+        }else{
+            forward=INSERT_OR_EDIT;
+        }
+        RequestDispatcher view=request.getRequestDispatcher(forward);
+        view.forward(request, response);
+    }
+
+    @Override
+    protected void doPost(HttpServletRequest request, HttpServletResponse response)
+            throws ServletException, IOException {
+               User user=new User();
+               user.setUname(request.getParameter("uname"));
+               user.setPassword(request.getParameter("pass"));
+               try {
+                   Date reg=new SimpleDateFormat("yyyy/MM/dd").parse(request.getParameter("dob"));
+                   user.setRegisteredon(reg);
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+               user.setEmail(request.getParameter("email"));
+               String userid=request.getParameter("uname");
+               user.setUname(userid);
+               dao.checkUser(user);
+               RequestDispatcher view=request.getRequestDispatcher(LIST_USER);
+               request.setAttribute("users", dao.getAllUsers());
+               view.forward(request, response);
+    }
+
+  
+}
